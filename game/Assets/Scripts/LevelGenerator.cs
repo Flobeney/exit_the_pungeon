b0263a5696a7291702a980b8f083c8f6a401f78a
Florent Beney <32325134+Flobeney@users.noreply.github.com>
2022-11-27 20:27:59 +0100

    Début génération de salle aléatoire

--- game/Assets/Scripts/LevelGenerator.cs
+++ game/Assets/Scripts/LevelGenerator.cs
@@ -6,6 +6,10 @@ public class LevelGenerator : MonoBehaviour
 {
     // Constants
     private const string MATERIALS_FOLDER = "LowlyPoly";
+    private const int PERCENT_CHANCE_DOOR = 10;
+    private const int SIZE_DOOR = 2;
+    private const int MAX_DOOR_PER_WALL = 2;
+    private const int SPACE_BETWEEN_DOOR = 3;
 
     // Champs
     public GameObject TilePrefab;
@@ -13,7 +17,7 @@ public class LevelGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GenerateWalls();
+        GenerateRoom();
     }
 
     // Update is called once per frame
@@ -22,7 +26,10 @@ public class LevelGenerator : MonoBehaviour
         
     }
 
-    void GenerateWalls(){
+    /// <summary>
+    /// Generates a room
+    /// </summary>
+    void GenerateRoom(){
         // Choose material
         Object[] materials = Resources.LoadAll(MATERIALS_FOLDER, typeof(Material));
         Material material = (Material)materials[Random.Range(0, materials.Length)];
@@ -33,35 +40,77 @@ public class LevelGenerator : MonoBehaviour
         Vector3 max = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, Camera.main.nearClipPlane));
         Vector3 sizeTile = TilePrefab.GetComponent<Renderer>().bounds.size;
 
-        // Haut & bas
-        for (float x = min.x; x < max.x; x += sizeTile.x){
-            // Haut
-            Instantiate(
-                TilePrefab, 
-                new Vector3(x + (sizeTile.x / 2), max.y - (sizeTile.y / 2), 0), 
-                Quaternion.identity
-            );
-            // Bas
-            Instantiate(
-                TilePrefab, 
-                new Vector3(x + (sizeTile.x / 2), min.y + (sizeTile.y / 2), 0), 
-                Quaternion.identity
-            );
+        // Haut
+        GenerateWall(min, max, sizeTile, true, true);
+        // Bas
+        GenerateWall(min, max, sizeTile, true, false);
+        // Gauche
+        GenerateWall(min, max, sizeTile, false, true);
+        // Droite
+        GenerateWall(min, max, sizeTile, false, false);
+    }
+
+    /// <summary>
+    /// Generates the walls of the level
+    /// </summary>
+    /// <param name="min">Min of the camera</param>
+    /// <param name="max">Max of the camera</param>
+    /// <param name="sizeTile">Size of the tile</param>
+    /// <param name="isHorizontal">If the wall is horizontal</param>
+    /// <param name="isLeft">If the wall is on the left</param>
+    void GenerateWall(Vector3 min, Vector3 max, Vector3 sizeTile, bool isHorizontal, bool isLeft){
+        // Nb de portes sur le mur actuel
+        int nbDoor = 0;
+        float lastDoorPosition = 0;
+        // Définir quelles variables utiliser suivant si c'est haut/bas ou gauche/droite
+        float start = isHorizontal ? min.x : min.y;
+        float limit = isHorizontal ? max.x : max.y;
+        float incr = isHorizontal ? sizeTile.x : sizeTile.y;
+
+        // Boucle
+        for (float i = start; i < limit; i += incr){
+            // Générer une porte (évt.)
+            if(
+                nbDoor < MAX_DOOR_PER_WALL // Pas trop de portes par mur
+                && i - lastDoorPosition > SPACE_BETWEEN_DOOR * incr // Pas des portes trop proches
+                && GenerateDoor(i, start, limit)
+            ){
+                // Incrémenter le nb de portes
+                nbDoor++;
+                // Sauter 2 cases
+                i += incr * SIZE_DOOR;
+                // Définir la position de la dernière porte
+                lastDoorPosition = i;
+                continue;
+            }
+            // Définir la position en fonction de si c'est haut/bas ou gauche/droite
+            Vector3 pos;
+            if(isHorizontal){
+                pos = isLeft 
+                ? new Vector3(i + (sizeTile.x / 2), max.y - (sizeTile.y / 2), 0) 
+                : new Vector3(i + (sizeTile.x / 2), min.y + (sizeTile.y / 2), 0);
+            }else{
+                pos = isLeft 
+                ? new Vector3(min.x + (sizeTile.x / 2), i + (sizeTile.y / 2), 0) 
+                : new Vector3(max.x - (sizeTile.x / 2), i + (sizeTile.y / 2), 0);
+            }
+            // Générer le mur
+            Instantiate(TilePrefab, pos, Quaternion.identity);
         }
-        // Gauche & droite
-        for (float y = min.y; y < max.y; y += sizeTile.y){
-            // Gauche
-            Instantiate(
-                TilePrefab, 
-                new Vector3(min.x + (sizeTile.x / 2), y + (sizeTile.y / 2), 0), 
-                Quaternion.identity
-            );
-            // Droite
-            Instantiate(
-                TilePrefab, 
-                new Vector3(max.x - (sizeTile.x / 2), y + (sizeTile.y / 2), 0), 
-                Quaternion.identity
-            );
+    }
+
+    /// <summary>
+    /// Generates a door
+    /// </summary>
+    /// <param name="i">Current position</param>
+    /// <param name="start">Start of the camera</param>
+    /// <param name="limit">Limit of the camera</param>
+    bool GenerateDoor(float i, float min, float max){
+        // Porte pour changer de niveau
+        if(i > min && i < max - SIZE_DOOR){ // S'assurer de pas être au 1er ou dernier tile
+            // Si le % est atteint
+            return Random.Range(0, 100) < PERCENT_CHANCE_DOOR;
         }
+        return false;
     }
 }
