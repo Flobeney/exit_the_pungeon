48d603487db52ea2b276201534ae6d38f54aa7b9
David Leal <david.leal299@gmail.com>
2022-12-02 09:49:12 +0100

    ça fonctionne maintenant

--- game/Assets/Scripts/GunController.cs
+++ game/Assets/Scripts/GunController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Aim : MonoBehaviour
+public class GunController : MonoBehaviour
 {
     private float aimAngle, timeBtwShots;
     private Vector2 aim;
