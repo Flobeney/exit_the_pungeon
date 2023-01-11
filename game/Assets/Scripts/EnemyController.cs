963a2460644c25d5cb9548769f669752d057965e
Florent Beney <32325134+Flobeney@users.noreply.github.com>
2023-01-11 12:02:48 +0100

    Faire bouger les ennemis sur le réseau

--- game/Assets/Scripts/EnemyController.cs
+++ game/Assets/Scripts/EnemyController.cs
@@ -11,17 +11,26 @@ public class EnemyController : NetworkBehaviour
     private Rigidbody2D rb;
     private Vector3 movement;
     private float speed = 5f;
+    private GameObject[] _players;
+    float IntervalMin = 5, IntervalMax = 7;
+    float interval, startTime, endTime;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        // Init interval
+        InitInterval();
     }
 
     // Update is called once per frame
     void Update()
     {
         if(IsHost || IsServer){
+            // Fin attente interval
+            if (Time.time >= endTime){
+                NewMovementServerRpc();
+            }
             UpdateServerRpc();
         }
     }
@@ -29,8 +38,13 @@ public class EnemyController : NetworkBehaviour
     [ServerRpc]
     void UpdateServerRpc(){
         // Faire la différence entre la position du joueur et de l'ennemi
-        movement = (Player.transform.position - this.transform.position) / 10;
+        // movement = (Player.transform.position - this.transform.position) / 10;
         rb.velocity = movement * speed;
+    }
+
+    [ServerRpc]
+    void NewMovementServerRpc(){
+        movement = new Vector3(Random.Range(-1.5f, 1.5f), Random.Range(-1.5f, 1.5f), 0);
     }
 
     // Lors d'une collision
@@ -50,5 +64,13 @@ public class EnemyController : NetworkBehaviour
             Destroy(this.gameObject);
             GameObject.Find("EnemyGenerator").GetComponent<EnemyGenerator>().EnemyDestroyed();
         }
+    }
+
+    // Init un nouvel interval
+    private void InitInterval()
+    {
+        interval = Random.Range(IntervalMin, IntervalMax);
+        startTime = Time.time;
+        endTime = startTime + interval;
     }
 }
