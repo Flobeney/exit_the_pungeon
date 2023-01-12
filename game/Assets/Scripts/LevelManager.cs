aa5c35701bb72211bc66d42362196f8f991cc55d
Florent Beney <32325134+Flobeney@users.noreply.github.com>
2023-01-12 10:57:07 +0100

    Cacher le canvas lors start partie

--- game/Assets/Scripts/LevelManager.cs
+++ game/Assets/Scripts/LevelManager.cs
@@ -9,9 +9,14 @@ public class LevelManager : NetworkBehaviour
     // Constantes
     const string NAME_SCENE_GAME = "LevelGenerator";
 
+    // Champs
+    public GameObject Canvas;
+
     // Chargement de la scène de jeu
     [ServerRpc]
     public void LoadSceneServerRpc(){
         NetworkManager.SceneManager.LoadScene(NAME_SCENE_GAME, LoadSceneMode.Additive);
+        // Cacher le canvas
+        Canvas.SetActive(false);
     }
 }
