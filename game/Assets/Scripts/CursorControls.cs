8d305c0883b9fe8f6b5c81d4afc3a55c39d567b7
David Leal <david.leal299@gmail.com>
2023-01-12 16:17:03 +0100

    Yes

--- game/Assets/Scripts/CursorControls.cs
+++ game/Assets/Scripts/CursorControls.cs
@@ -1,21 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class CursorControls : MonoBehaviour
+public class CursorControls : NetworkBehaviour
 {
     private Vector2 mousePos;
 
     // Start is called before the first frame update
     void Awake()
     {
-        Cursor.visible = false;
+        //Cursor.visible = false;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+<<<<<<< Updated upstream
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+=======
+        if (!IsServer) return;
+        Camera currentCam = Camera.current != null ? Camera.current : Camera.main;
+        mousePos = currentCam.ScreenToWorldPoint(Input.mousePosition);
+>>>>>>> Stashed changes
         transform.position = mousePos;
     }
 }
--- game/Assets/Scripts/GunController.cs
+++ game/Assets/Scripts/GunController.cs
@@ -14,13 +14,21 @@ public class GunController : MonoBehaviour
     void Awake()
     {
         aim = new Vector2();
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+<<<<<<< Updated upstream
         // Rotation towards the mouse
         aim = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+=======
+        if (!IsOwner) return;
+
+        // Rotation towards the cursor
+        aim = cursor.transform.position - transform.position;
+>>>>>>> Stashed changes
         aimAngle = Mathf.Atan2(aim.y, aim.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(aimAngle, Vector3.forward);
 
@@ -37,4 +45,30 @@ public class GunController : MonoBehaviour
             timeBtwShots -= Time.deltaTime;
         }
     }
+<<<<<<< Updated upstream
+=======
+
+    [ServerRpc]
+    private void ShootServerRpc()
+    {
+        GameObject go =
+            Instantiate(bullet,
+            shootPoint.transform.position,
+            transform.rotation);
+        spawnedBullets.Add(go);
+        go.GetComponent<BulletController>().parent = this;
+        go.GetComponent<NetworkObject>().Spawn();
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void DestroyBulletServerRpc()
+    {
+        GameObject toDestroy = spawnedBullets[0];
+        toDestroy.GetComponent<NetworkObject>().Despawn();
+        spawnedBullets.Remove(toDestroy);
+        Destroy(toDestroy);
+    }
+
+    // TODO: ServerRpc for particles and probably animations
+>>>>>>> Stashed changes
 }
